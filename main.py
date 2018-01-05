cb4ffe1fe001588f1304d58ca3f2983edd5dae28
Acheron <acheron.igor.b@gmail.com>
2018-01-05 12:52:16 +0200

    find_bukl now working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from collections import defaultdict
 import datetime
 import asyncio
+import json
 
 from session import GSession
 
@@ -21,7 +22,9 @@ class Scraper(object):
     LIC_TYPES = defaultdict(lambda : 'Unknown')
     LIC_TYPES.update({'12': 'Local', '13': 'Network'})
 
-    def __init__(self, raise_exceptions=True):
+    def __init__(self, coros_limit=100, r_timeout=5, raise_exceptions=True):
+        self.r_timeout = r_timeout
+        self.coros_limit = coros_limit
         self.raise_exceptions = raise_exceptions
 
     def find_one(self, org_code: str):
@@ -45,16 +48,47 @@ class Scraper(object):
     def find_bulk(self, org_codes: Iterable):
         assert hasattr(org_codes, '__iter__'), 'Container for org_codes should be iterable'
 
+        results = []
+
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
-        result = loop.run_until_complete(self._run_bulk())
+        results_raw = loop.run_until_complete(self._run_bulk(org_codes))
 
         loop.close()
 
-    async def _run_bulk(self):
+        for item in results_raw:
+            for org_code, data_raw in item.items():
+                data = self._process(data_raw)
+                data['org_code'] = org_code
+                results.append(data)
+
+        return results
+
+    async def _run_bulk(self, org_codes):
         # TODO: add headers
         session = GSession()
+        semaphore = asyncio.Semaphore(self.coros_limit)
+
+        tasks, result = [], []
+
+        for code in org_codes:
+            tasks.append(self._get_data(code, session, semaphore))
+
+        result = await asyncio.gather(*tasks, return_exceptions=True)
+
+        session.close()
+        return result
+
+    async def _get_data(self, org_code, session, semaphore):
+        data = {org_code: []}
+        req_params = {'edrpo': org_code, 'type': 'json'}
+
+        response = await session.get(self.BASE_URL, params=req_params, semaphore=semaphore, timeout=self.r_timeout)
+        if response.content:
+            data_raw = json.loads(response.content)
+            data = {org_code: data_raw}
+        return data
 
     def _process(self, data):
 
@@ -93,4 +127,6 @@ class Scraper(object):
 
 
 a = Scraper()
-a.find_one('38345394')
+# a.find_one('38345394')
+r = a.find_bulk(['38345394', '12345678', 'x2343'])
+print(r)
